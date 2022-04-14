40ea3b95ea9f79367debb3ee3a865336087e7baf
Angus Hollands <goosey15@gmail.com>
2022-04-14 13:53:06 +0100

    Refactor: simplify rendering code

--- src/index.ts
+++ src/index.ts
@@ -22,48 +22,44 @@ export default function mermaid(md: MarkdownIt, options: any) {
         let token = tokens[idx];
         let info = token.info.trim();
         let langName = info ? getLangName(info) : "";
+
+        if (langName !== "mermaid")
+        {
+            if (defaultFenceRenderer !== undefined) {
+                return defaultFenceRenderer(tokens, idx, options, env, slf);
+            }
+            // Missing fence renderer!
+            return "";
+        }
+
         let imageHTML: string = "";
         let imageAttrs: string[][] = [];
 
-        // Only handle custom token
-        switch (langName) {
-            case "mermaid": {
-                const element = document.createElement("div");
-                document.body.appendChild(element);
+        // Create element to render into
+        const element = document.createElement("div");
+        document.body.appendChild(element);
 
-                // Render with Mermaid
-                try {
-                    const container_id = "mermaid-container";
-                    Mermaid.mermaidAPI.render(container_id, token.content, (html: string) => {
-                        // We need to forcibly extract the max-width/height attributes to set on img tag
-                        let svg = document.getElementById(container_id);
-                        if (svg !== null) {
-                            imageAttrs.push(["style", `max-width:${svg.style.maxWidth};max-height:${svg.style.maxHeight}`]);
-                        }
-                        // Store HTML
-                        imageHTML = html;
-                    }, element);
-                } catch (e) {
-                    return `<div class="alert alert-danger">${e.toString()}</div>`;
-                } finally {
-                    element.remove();
+        // Render with Mermaid
+        try {
+            const container_id = "mermaid-container";
+            Mermaid.mermaidAPI.render(container_id, token.content, (html: string) => {
+                // We need to forcibly extract the max-width/height attributes to set on img tag
+                let svg = document.getElementById(container_id);
+                if (svg !== null) {
+                    imageAttrs.push(["style", `max-width:${svg.style.maxWidth};max-height:${svg.style.maxHeight}`]);
                 }
-
-                // Store encoded image data
-                imageAttrs.push(["src", `data:image/svg+xml,${encodeURIComponent(imageHTML)}`]);
-                return `<img ${slf.renderAttrs({attrs: imageAttrs})}>`;
-            }
-            default: {
-                if (defaultFenceRenderer !== undefined) {
-                    return defaultFenceRenderer(tokens, idx, options, env, slf);
-                }
-                // Missing fence renderer!
-                return "";
-            }
-
+                // Store HTML
+                imageHTML = html;
+            }, element);
+        } catch (e) {
+            return `<div class="alert alert-danger">${e.toString()}</div>`;
+        } finally {
+            element.remove();
         }
 
-
+        // Store encoded image data
+        imageAttrs.push(["src", `data:image/svg+xml,${encodeURIComponent(imageHTML)}`]);
+        return `<img ${slf.renderAttrs({attrs: imageAttrs})}>`;
     }
 
     md.renderer.rules.fence = customFenceRenderer;
